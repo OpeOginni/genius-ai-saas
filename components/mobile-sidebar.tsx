ba6e005fbc3d6204391887ed47a7b3c1cb97c879
OpeOginni <brightoginni123@gmail.com>
2023-08-15 16:43:52 +0100

    Completed Conversation AI Part using OpenAI API

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Fix for components that cause hydration ERROR
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
