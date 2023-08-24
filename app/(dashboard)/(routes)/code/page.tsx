ff038992616a38bac3e1f0dcb5642691ed069975
OpeOginni <brightoginni123@gmail.com>
2023-08-24 16:03:47 +0100

    Complete Code Generator

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import * as z from "zod";
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import OpenAI from "openai";
+import ReactMarkdown from "react-markdown";
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -21,7 +22,7 @@ import { Loader } from "@/components/loader";
 
 import { formSchema } from "./constants";
 
-const ConversationPage = () => {
+const CodePage = () => {
   const router = useRouter();
   const [messages, setMessages] = useState<
     OpenAI.Chat.CreateChatCompletionRequestMessage[]
@@ -44,7 +45,7 @@ const ConversationPage = () => {
       };
       const newMessages = [...messages, userMessage];
 
-      const response = await axios.post("/api/conversation", {
+      const response = await axios.post("/api/code", {
         messages: newMessages,
       });
 
@@ -62,11 +63,11 @@ const ConversationPage = () => {
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation model yet."
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Code Generation"
+        description="Generate code using descriptive text."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
       <div className="px-4 lg:px-8">
         <div>
@@ -83,7 +84,7 @@ const ConversationPage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="How do I calculate the area of a Triangle"
+                        placeholder="Simple toggle button using react hooks."
                         {...field}
                       />
                     </FormControl>
@@ -120,7 +121,21 @@ const ConversationPage = () => {
                 )}
               >
                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className="text-sm overflow-hidden leading-7"
+                >
+                  {message.content || ""}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
@@ -130,4 +145,4 @@ const ConversationPage = () => {
   );
 };
 
-export default ConversationPage;
+export default CodePage;
--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -7,11 +7,14 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const instructionMessage: OpenAI.Chat.CreateChatCompletionRequestMessage = {
+    role: "system",
+    content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations"
+}
 
 export async function POST(
     req: Request
 ) {
-
     try {
         const { userId } = auth()
         const body = await req.json();
@@ -31,13 +34,13 @@ export async function POST(
 
         const response = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
-            messages
+            messages: [instructionMessage, ...messages]
         })
 
         return NextResponse.json(response.choices[0].message)
 
     } catch (error) {
-        console.log('[CONVERSATION_ERROR]', error)
+        console.log('[CODE_ERROR]', error)
         return new NextResponse("Internal error", { status: 500 })
     }
 }
